8c539375f561ae33ff2ace043cbe8a739301b47b
이재현 <fanta4715@naver.com>
2024-01-16 01:05:22 +0900

    init

---
+++ src/main/java/com/example/solasido/SolasidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.solasido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolasidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolasidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solasido/SolasidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solasido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolasidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
