85ddb4baa6d782108124dc6a2254e461f4f246c4
이재현 <fanta4715@naver.com>
2024-01-17 00:02:51 +0900

    feat: test controller 추가

---
+++ src/main/java/com/example/solasido/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.solasido.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "EC2 배포 테스트";
+    }
+}
